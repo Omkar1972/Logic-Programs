2aa7ac893c7fca213a89b178ff3371398637a947
Omkar <omkarbagmare49@gmail.com>
2025-11-10 14:46:22 +0530

    half pyramid

---
+++ HalfPyramid.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HalfPyramid {
+	
+    public static void main(String[] args) {
+    	
+    	Scanner s=new Scanner(System.in);
+    	
+    	System.out.print("Enter number:-");
+        int r = s.nextInt();
+        
+        for (int i = 1; i <= r; i++) {
+        	
+            for (int j = 1; j <= i; j++)
+            	
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
