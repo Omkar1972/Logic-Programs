a1184df24daedc7a6d0e406e9485f1a947a9ac47
Omkar <omkarbagmare49@gmail.com>
2025-11-17 15:35:21 +0530

    modi , number

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-public class HalfPyramid {
+public class NumberPyramid {
 	
     public static void main(String[] args) {
     	
     	Scanner s=new Scanner(System.in);
     	
-    	System.out.print("Enter number:-");
+    	System.out.println("Enter number:-");
         int r = s.nextInt();
         
         for (int i = 1; i <= r; i++) {
         	
             for (int j = 1; j <= i; j++)
             {
-            	 System.out.print("* ");
+            	 System.out.print(j + " ");
             }
-            	 
+               
             System.out.println();
         }
     }
